7720912d92c4e7af5f0f61673574c73a2b3c21cb
Fdamirez <fabianramirez0072@gmail.com>
2022-12-06 20:24:53 -0500

    feat entity finish

---
+++ Entity/Enum/PurchaseState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Enum
+{
+    internal class Class1
+    {
+    }
+}
---
+++ Entity/User.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+    }
+}
